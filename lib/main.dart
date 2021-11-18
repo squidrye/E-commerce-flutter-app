b9ecd3ff8f3b6f7f53b751967f353a8b24dea063
squidrye <ancientphoebus@gmail.com>
2021-11-18 22:24:05 +0530

    add providers and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:shopapp/providers/product_list_provider.dart';
+import 'package:shopapp/screens/CartScreen.dart';
+import 'package:shopapp/screens/OrderScreen.dart';
 import 'screens/AllProductScreen.dart';
 import 'package:provider/provider.dart';
 import 'screens/OneProductScreen.dart';
+import 'providers/cart_map_provider.dart';
+import 'providers/order_provider.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext ctx) {
-         return ProductList();
-      },
+    return MultiProvider(
+      providers:[
+        ChangeNotifierProvider(create: (BuildContext ctx){
+          return ProductList();
+        }),
+        ChangeNotifierProvider(create:(BuildContext ctx){
+          return CartProvider();
+        }),
+        ChangeNotifierProvider(create:(BuildContext ctx){
+          return Order();
+        }),
+      ],
       child:MaterialApp(
           title: "ShopApp",
           theme: ThemeData(
-            primaryColor: Colors.purple,
+            primarySwatch: Colors.purple,
             accentColor: Colors.red,
           ),
           home: AllProductsScreen(),
@@ -24,6 +36,12 @@ class MyApp extends StatelessWidget {
             OneProductScreen.routeName:(BuildContext ctx){
               return OneProductScreen();
             },
+            CartScreen.routeName:(BuildContext ctx){
+              return CartScreen();
+            },
+            OrderScreen.routeName:(BuildContext ctx){
+              return OrderScreen();
+            }
           }
         ),
     );
