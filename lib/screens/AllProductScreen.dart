f4fc866341efcce42b4669af61b59469d91db890
squidrye <ancientphoebus@gmail.com>
2021-11-18 22:31:44 +0530

    overview screen

--- lib/screens/AllProductScreen.dart
+++ lib/screens/AllProductScreen.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:shopapp/providers/cart_map_provider.dart';
+import 'package:shopapp/screens/CartScreen.dart';
+import 'package:shopapp/widget/AppDrawer.dart';
+import 'package:shopapp/widget/Badge.dart';
 import '../widget/AllItemsGrid.dart';
-class AllProductsScreen extends StatelessWidget{
-  Widget build(BuildContext context){
+import 'package:provider/provider.dart';
+
+enum FilterValue {
+  Favourite,
+  All,
+}
+
+class AllProductsScreen extends StatefulWidget {
+  AllProductsScreenState createState() {
+    return AllProductsScreenState();
+  }
+}
+
+class AllProductsScreenState extends State<AllProductsScreen> {
+  var _showOnlyFavs = false;
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title:Text("Shop"),
-      ),
-      body:AllItemsGrid(),
+      appBar: AppBar(title: Text("Shop"), actions: <Widget>[
+        PopupMenuButton(
+            onSelected: (FilterValue value) {
+              setState(() {
+                if (value == FilterValue.Favourite) {
+                  _showOnlyFavs = true;
+                } else
+                  _showOnlyFavs = false;
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) {
+              return [
+                PopupMenuItem(
+                  child: Text("Favourites"),
+                  value: FilterValue.Favourite,
+                ),
+                PopupMenuItem(
+                  child: Text("All Items"),
+                  value: FilterValue.All,
+                ),
+              ];
+            }),
+        Consumer<CartProvider>(
+          builder:(_,cartInstance,ch){
+            return Badge(
+            itemCount: cartInstance.getItemCount(),
+            widget: IconButton(
+              icon: Icon(Icons.shopping_cart_rounded),
+              onPressed: () {
+                Navigator.pushNamed(context,CartScreen.routeName);
+              },
+            ),
+          );
+          }
+        )
+      ]),
+      drawer:AppDrawer(),
+      body: AllItemsGrid(_showOnlyFavs),
     );
   }
 }
